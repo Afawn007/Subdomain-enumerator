d518b8cf95f9678f9fe3ab5ed4cb51be213781b0
Afawn007 <afawnkanth@gmail.com>
2026-02-16 21:10:36 +0530

    Update subdomain-enumerator.py

--- subdomain-enumerator.py
+++ subdomain-enumerator.py
@@ -8,10 +8,10 @@ parser.add_argument('--threads', type=int,help='Number of threads', default=10)
 args = parser.parse_args()
 domain = args.d.replace("http://", "").replace("https://", "").strip("/")# ye chu incase user enters http:// . We remove it 
 wordlist = args.w
-max_threads = args.threads
+maxthreads = args.threads
 with open(wordlist) as file: # We open the wordlist
     subdomains = file.read().splitlines() # Take all subdomain in wordlist
-def check_subdomain(subdomain): #  Main function ju chu check kran all subdomains
+def domaincheckkk(subdomain): #  Main function ju chu check kran all subdomains
     url = f"https://{subdomain}.{domain}" # ye chu url banawan jemis sozav es request
     try:
         response = requests.get(url, timeout=3) # sending actual requests
@@ -27,8 +27,8 @@ def check_subdomain(subdomain): #  Main function ju chu check kran all subdomain
             return f"[{response.status_code}] {url}"
     except requests.RequestException:
         return None
-with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor: # Actual threads
-    futures = [executor.submit(check_subdomain,d)for d in subdomains] # Loop through subdomains in worlist anf give them to main  function
+with concurrent.futures.ThreadPoolExecutor(max_workers=maxthreads) as executor: # Actual threads
+    futures = [executor.submit(domaincheckkk,d)for d in subdomains] # Loop through subdomains in worlist anf give them to main  function
     for future in concurrent.futures.as_completed(futures): # Check the completed threads and block code completion until all threads run
         try:
             result = future.result() # Store results and print if result does not have none
